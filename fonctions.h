b9ef5670cd8520195b0389a71eaebfc98a4a598c
Ziedkamoun <47940170+Ziedkamoun@users.noreply.github.com>
2019-03-26 13:04:28 +0100

    Rename fonction.h to fonctions.h

---
+++ fonctions.h
@@ -0,0 +1,18 @@
+#ifndef MENU2_H_INCLUDED
+#define MENU2_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+
+
+
+
+void menu2(SDL_Surface* ecran);
+void menu1(SDL_Surface* ecran);
+void settings(SDL_Surface* ecran);
+void sound(SDL_Surface* ecran);
+#endif // MENU2_H_INCLUDED
