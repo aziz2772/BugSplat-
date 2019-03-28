4d75af94e2ec118fbe43496af7dbf4c75b352824
Ziedkamoun <47940170+Ziedkamoun@users.noreply.github.com>
2019-03-29 00:39:49 +0100

    Create main.c

---
+++ Jeux Zied Version 0.1/main.c
@@ -0,0 +1,28 @@
+
+#include "defs.h"
+
+
+void main()
+
+{
+SDL_Surface *ecran =NULL, *cape =NULL;
+menu1(ecran);
+
+
+       // aprés apuis des boutons
+     //  positionFond.x = 500;
+       // positionFond.y = 360;
+        //SDL_BlitSurface(texte, NULL, ecran, &positionFond); /* Blit du texte */
+        //SDL_Flip(ecran);
+
+     
+
+
+
+
+    SDL_Quit();
+
+
+    return;
+
+}
