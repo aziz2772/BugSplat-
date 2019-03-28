b7e6199c1e4e0c7eb4c1962f79a20c160cbac712
Ziedkamoun <47940170+Ziedkamoun@users.noreply.github.com>
2019-03-29 00:45:51 +0100

    Create draw.h

---
+++ Jeux Zied Version 0.1/draw.h
@@ -0,0 +1,7 @@
+  #include "structs.h"
+  /* Prototypes des fonctions utilisées */
+   extern void drawMap(void);
+ extern void drawplayer(void);
+  extern Gestion jeu;
+  extern Map map;
+ extern void drawanimatedplayer(void);
